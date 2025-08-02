ce6eb282af1f9ff158de7a6fa47fffe00a8e3912
sannabewaga <113686593+sannabewaga@users.noreply.github.com>
2025-08-02 11:53:05 +0530

    Create ranking_queries.sql

---
+++ basic_eda/ranking_queries.sql
@@ -0,0 +1,45 @@
+-- ============================================
+-- Query 1: Revenue by Product Category
+-- ============================================
+
+SELECT 
+    p.category,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+GROUP BY p.category
+ORDER BY total_revenue DESC;
+
+
+-- ============================================
+-- Query 2: Top 5 Products by Revenue
+-- ============================================
+
+SELECT TOP 5
+    p.product_name,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC;
+
+
+-- ============================================
+-- Query 3: Top 3 Customers by Total Orders
+-- ============================================
+
+SELECT TOP 3
+    c.customer_key,
+    c.first_name,
+    c.last_name,
+    COUNT(DISTINCT order_number) AS total_orders
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+GROUP BY
+    c.customer_key,
+    c.first_name,
+    c.last_name
+ORDER BY total_orders DESC;
